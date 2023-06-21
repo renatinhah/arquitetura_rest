98cb1a20ca37638f80b94a9894989562e79dc461
renatinhah <renata.ufop@yahoo.com.br>
2023-06-21 19:15:02 -0300

    first commit

---
+++ src/main/java/com/renata/arquitetura_rest/ArquiteturaRestApplication.java
@@ -0,0 +1,13 @@
+package com.renata.arquitetura_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renata/arquitetura_rest/ArquiteturaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renata.arquitetura_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
